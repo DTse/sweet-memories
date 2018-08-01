6f3b824670ffe456ed39d8a37b84973802903cfd
Tsetsonis Dimitris <jimtse12@gmail.com>
2018-08-02 01:56:40 +0300

    Webp Images

---
+++ webp.js
@@ -0,0 +1,13 @@
+var imagemin = require("imagemin"),    // The imagemin module.
+  webp = require("imagemin-webp"),   // imagemin's WebP plugin.
+//   outputFolder = "./src/images",        // PNG images
+//   JPEGImages = "./src/images/*.jpg";        // JPEG images
+
+  outputFolder = "./static/images/",        // PNG images
+  JPEGImages = "./static/images/*.jpg";        // JPEG images
+
+imagemin([JPEGImages], outputFolder, {
+  plugins: [webp({
+    quality: 65 // Quality setting from 0 to 100
+  })]
+});
