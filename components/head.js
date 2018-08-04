34985e01dfddc05aa46559eca49f55bb75261984
Tsetsonis Dimitris <jimtse12@gmail.com>
2018-08-04 13:12:06 +0300

    Meta setup

--- components/head.js
+++ components/head.js
@@ -1,19 +1,27 @@
 import NextHead from 'next/head'
 import { string } from 'prop-types'
 
-const defaultDescription = ''
-const defaultOGURL = ''
-const defaultOGImage = ''
+const defaultDescription = 'H ομάδα του «Sweet Memory» θα πραγματοποιήσει οποιαδήποτε ανάγκη και «κρυφό» σας θέλω αφήνοντας την κάθε σας στιγμή, ανεξίτηλη στο χρόνο!!!'
+const defaultOGURL = 'https://sweet-memory.gr/'
+const defaultOGImage = 'https://sweet-memory.gr/static/images/large-image.jpg'
+const defaultTitle = 'Sweet Memory | Γάμος, Βάπτιση, Bachelor, Events!'
 
 const Head = (props) => (
   <NextHead>
     <meta charset="UTF-8" />
-    <title>{props.title || ''}</title>
+    <title>{props.title || defaultTitle}</title>
     <meta name="description" content={props.description || defaultDescription} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="194x194" href="/static/favicon-194x194.png"/>
+    <link rel="icon" type="image/png" sizes="192x192" href="/static/android-chrome-192x192.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
+    <link rel="manifest" href="/static/site.webmanifest"/>
+    <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#a88888"/>
+    <meta name="msapplication-TileColor" content="#ffffff"/>
+    <meta name="msapplication-TileImage" content="/static/mstile-144x144.png"/>
+    <meta name="theme-color" content="#ffffff"/>
     <link rel="icon" href="/static/favicon.ico" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
@@ -24,6 +32,38 @@ const Head = (props) => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+    <meta name="og:site_name" content= "Sweet Memory"/>
+    <meta name="subject" content= "Γάμος, Βάπτιση, Bachelor, Events" />
+    <meta name="language" content="el_GR"/>
+    <meta name="robots" content="index,follow"/>
+    <meta name="Classification" content="Business"/>
+    <meta name="author" content= "Sweet Memory, info@sweet-memory.gr"/>
+    <meta name="designer" content="Δημήτρης Τσετσώνης"/>
+    <meta name="copyright" content= "Sweet Memory 2018"/>
+     <script type="application/ld+json">{`
+                                {
+                                    "@context": "http://schema.org",
+                                    "@type": "LocalBusiness",
+                                    "contactPoint": [
+                                        { "@type": "ContactPoint",
+                                          "telephone": " +30-698-943-3978",
+                                          "contactType": "sales"
+                                        }
+                                      ],
+                                    "description": "${props.description || defaultDescriptio}",
+                                    "name": "Sweet Memory",
+                                    "image": "${defaultOGImage}",
+                                    "telephone": "+30-210-3224460",
+                                    "openingHours": "Mo,Tu,We,Th,Fr 09:00-21:00",
+                                    "@id": "https://sweet-memory.gr/",
+                                    "url": "https://sweet-memory.gr//",
+                                    "inLanguage": "el",
+                                    "sameAs" : [ 
+                                        "https://instagram.com/sweetmemoryevents",
+                                        "https://www.facebook.com/sweetmemoryevents"
+                                    ]
+                                }
+                            `}</script>
   </NextHead>
 )
 
